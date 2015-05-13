3b3279a4b603b0134ba871952485af01f1cc0d35
ligia0119 <ligia0119@gmail.com>
2015-05-14 04:19:35 +0800

    run test plan and calculate score

--- JMeter_core/src/nju/edu/cn/mooctest/net/plugin/scriptprocessor/impl/ScriptProcessorImpl.java
+++ JMeter_core/src/nju/edu/cn/mooctest/net/plugin/scriptprocessor/impl/ScriptProcessorImpl.java
@@ -4,14 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-import org.json.JSONObject;
-
+import nju.edu.cn.mooctest.jmeter.test.TestPlanEvaluation;
 import nju.edu.cn.mooctest.net.plugin.common.ActionMode;
 import nju.edu.cn.mooctest.net.plugin.common.Constants;
 import nju.edu.cn.mooctest.net.plugin.scriptprocessor.ScriptProcessor;
 
-public class ScriptProcessorImpl implements ScriptProcessor {
+import org.json.JSONObject;
 
+public class ScriptProcessorImpl implements ScriptProcessor {
+	private TestPlanEvaluation testPlanEvaluation;
+	
+	public ScriptProcessorImpl(TestPlanEvaluation testPlanEvaluation) {
+		this.setTestPlanEvaluation(testPlanEvaluation);
+	}
+	
 	@Override
 	public JSONObject process(String scriptURL, ActionMode mode)
 			throws Exception {
@@ -20,7 +26,11 @@ public class ScriptProcessorImpl implements ScriptProcessor {
 		JSONObject scoreJson = new JSONObject();
 
 		// read problem info from c:/mooctest-jmeter/projects/pro.mt
+		//TODO
 		File proInfoFile = new File(Constants.DOWNLOAD_PATH + "pro.mt");
+		if (!proInfoFile.exists()) {
+			proInfoFile.createNewFile();
+		}
 		BufferedReader bw = new BufferedReader(new FileReader(proInfoFile));
 		String proIdStr = bw.readLine();
 		String proNameStr = bw.readLine();
@@ -32,11 +42,14 @@ public class ScriptProcessorImpl implements ScriptProcessor {
 		scoreJson.put("sub_id", subIdStr);
 		scoreJson.put("eval_standard_id", evalStandardIdStr);
 
-		float finalScore = new Float(0.0);
-		// caculate score
-		finalScore = CaculateScore();
-		// get the score by a given strategy of calculation
-		scoreJson.put("score", new Float(finalScore).toString());
+//		float finalScore = new Float(0.0);
+//		// caculate score
+//		finalScore = CaculateScore();
+//		// get the score by a given strategy of calculation
+//		scoreJson.put("score", new Float(finalScore).toString());
+		
+		JSONObject score = testPlanEvaluation.calcuateScore();
+		scoreJson.put("score", score);
 
 		processDataJson.put("score", scoreJson);
 
@@ -52,4 +65,7 @@ public class ScriptProcessorImpl implements ScriptProcessor {
 		return finalScore;
 	}
 
+	public void setTestPlanEvaluation(TestPlanEvaluation testPlanEvaluation) {
+		this.testPlanEvaluation = testPlanEvaluation;
+	}
 }
